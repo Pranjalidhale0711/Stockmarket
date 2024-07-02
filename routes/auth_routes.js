45e4de7a1701af233f71bf64f7ae0f39cb159dff
Pranjali Dhale <pranjalidhale@Pranjalis-MacBook-Pro.local>
2024-07-03 00:38:12 +0530

    minor bug fixes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { getUserDetails, signIn,signUp, updateUsername} from '../controllers/auth_controllers.js'
 import {fetchUser}  from '../controllers/auth_controllers.js';
-import { checkAuthorization } from '../middleware/ValidateUser.js';
+import { checkAuthorization } from '../middleware/VerifyUser.js';
 
 
 const router=express.Router();
